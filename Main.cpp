a012c70082bdaa1246effb30f32428bd82a2bab2
ErickMartinez2 <erickmartinez2203@gmail.com>
2017-08-25 14:20:10 -0600

    Compilacion

--- Main.cpp
+++ Main.cpp
@@ -22,42 +22,42 @@ int main() {
 		cin >> oipcion;
 		cout << endl;
 		switch (opcion) {
-			case 1:
-				cout << "-> Agregar Matrices" << endl;
-				cout << "Ingrese el numero de filas de la matriz: ";
-				int filas;
-				cin >> filas;
-				cout << "Ingrese el numero de columnas de la matriz: ";
-				int columnas;
-				cin >> columnas;
-				Matriz matriz(filas, columnas);
-				matrices.push_back(matriz);
-				cout << "Matriz Creada!" << endl;
-				break;
-			case 2:
+			case 1: {
+						cout << "-> Agregar Matrices" << endl;
+						cout << "Ingrese el numero de filas de la matriz: ";
+						int filas;
+						cin >> filas;
+						cout << "Ingrese el numero de columnas de la matriz: ";
+						int columnas;
+						cin >> columnas;
+						Matriz matriz(filas, columnas);
+						matrices.push_back(matriz);
+						cout << "Matriz Creada!" << endl;
+					} break;
+			case 2: {
 
-				break;
-			case 3:
+					} break;
+			case 3: {
 
-				break;
-			case 4:
+					}break;
+			case 4: {
 
-				break;
-			case 5:
+					} break;
+			case 5: {
 
-				break;
-			case 6:
+					} break;
+			case 6: {
 
-				break;
-			case 7:
+					} break;
+			case 7: {
 
-				break;
-			case 8:
+					} break;
+			case 8: {
 
-				break;
-			case 9:
-				cout << "Hasta Pronto!!" << endl;
-				break;
+					} break;
+			case 9: {
+						cout << "Hasta Pronto!!" << endl;
+					} break;
 		}
 	} while (opcion != 9);
 	return 0;
