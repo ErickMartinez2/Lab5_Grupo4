a3f78f3778efbc8fb57b3a5a18eba9896850e923
dennisalejandro <dennisalenjandro@unitec.edu>
2017-08-25 15:02:21 -0600

    Matrices 5.0

--- Matriz.cpp
+++ Matriz.cpp
@@ -2,52 +2,37 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-Matriz::Matriz(int Height, int Width) {
-	this->Pointer = new int*[Height];
-	for (int i = 0;i<Height;i++) {
-		Pointer[i] = new int [Width];
-	}
-	int contador = 1;
-	for (int i = 0;i<Height;i++) {
-		for (int j = 0;j<Width;j++) {
-			int Num;
-			cout << "Ingrese el " << contador << " valor de la matriz: ";				
-			cin >> Num;
-			Pointer[i][j] = Num;
-			contador++;
+	Matriz::Matriz(int Height, int Width) {
+		this->Pointer = new int*[Height];
+		for (int i = 0;i<Height;i++) {
+			Pointer[i] = new int [Width];
 		}
+		int contador = 1;
+		for (int i = 0;i<Height;i++) {
+			for (int j = 0;j<Width;j++) {
+				int Num;
+				cout << "Ingrese el " << contador << " valor de la matriz: ";				
+				cin >> Num;
+				Pointer[i][j] = Num;
+				contador++;
+			}
+		}
+	}	
+	void Matriz::setHeight(int a) {
+		this->Height = a;
 	}
-}	
-
-void Matriz::setHeight(int a) {
-	this->Height = a;
-}
-
-void Matriz::setWidth(int a) {
-	this->Width = a;
-}
-
-void Matriz::setPointer(int** a) {
-	this->Pointer = a;
-}
-
-int Matriz::getHeight() {
-	return this->Height;
-}
-
-int Matriz::getWidth() {
-	return this->Width;
-}
-
-int** Matriz::getPointer() {
-	return this->Pointer;
-}
-
-Matriz::~Matriz() {
-	for (int i = 0; i < Height; i++) {
-		delete[] Pointer[i];
-		Pointer[i] = NULL;
+	void Matriz::setWidth(int a) {
+		this->Width = a;
 	}
-	delete Pointer;
-}
+	void Matriz::setPointer(int** a) {
+		this->Pointer = a;
+	}
+	int Matriz::getHeight() {
+		return this->Height;
+	}
+	int Matriz::getWidth() {
+		return this->Width;
+	}
+	int** Matriz::getPointer() {
+		return this->Pointer;
+	}
